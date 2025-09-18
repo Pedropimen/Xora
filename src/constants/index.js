4aa90ab514dc172be64a7f61e33e1b31999b82b3
Pedro Pimentel <pedropimetel900@gmail.com>
2025-09-18 00:27:37 -0300

    Primeiro commit projeto Xora

---
+++ src/constants/index.js
@@ -0,0 +1,158 @@
+export const features = [
+  {
+    id: "0",
+    icon: "/images/feature-1.png",
+    caption: "Integração fácil",
+    title: "Trabalhe de forma mais inteligente, não mais difícil",
+    text: "Com o Xora, tarefas tediosas são coisa do passado. Automação e processos inteligentes elevam sua produtividade a novos patamares. É como ter uma xícara extra de café, mas sem a ansiedade.",
+    button: {
+      icon: "/images/magictouch.svg",
+      title: "Assista à demonstração",
+    },
+  },
+  {
+    id: "1",
+    icon: "/images/feature-2.png",
+    caption: "Seguro e confiável",
+    title: "Durma tranquilo, nós cuidamos de você",
+    text: "A segurança dos seus dados é nossa prioridade. Com criptografia de última geração e controles robustos de privacidade, o Xora ajuda a manter suas informações seguras e mais protegidas que Fort Knox.",
+    button: {
+      icon: "/images/docs.svg",
+      title: "Leia a documentação",
+    },
+  },
+];
+
+export const details = [
+  {
+    id: "0",
+    icon: "/images/detail-1.png",
+    title: "Edição de vídeo automatizada por IA",
+  },
+  {
+    id: "1",
+    icon: "/images/detail-2.png",
+    title: "Colabore com sua equipe",
+  },
+  {
+    id: "2",
+    icon: "/images/detail-3.png",
+    title: "Motor em nuvem ultrarrápido",
+  },
+  {
+    id: "3",
+    icon: "/images/detail-4.png",
+    title: "Suporte ao cliente 24/7",
+  },
+];
+
+export const faq = [
+  {
+    id: "0",
+    question: "Quão fácil é configurar o Xora?",
+    answer:
+      "Com certeza! Você pode atualizar seu plano a qualquer momento e ainda recebe um desconto proporcional, garantindo o máximo valor da sua assinatura.",
+  },
+  {
+    id: "1",
+    question: "Posso integrar o Xora com outras plataformas?",
+    answer:
+      "Com certeza! Você pode atualizar seu plano a qualquer momento e ainda recebe um desconto proporcional, garantindo o máximo valor da sua assinatura.",
+  },
+  {
+    id: "2",
+    question: "Com que frequência vocês adicionam novos conteúdos?",
+    answer:
+      "Com certeza! Você pode atualizar seu plano a qualquer momento e ainda recebe um desconto proporcional, garantindo o máximo valor da sua assinatura.",
+  },
+  {
+    id: "3",
+    question: "Qual é a política de reembolso?",
+    answer:
+      "Com certeza! Você pode atualizar seu plano a qualquer momento e ainda recebe um desconto proporcional, garantindo o máximo valor da sua assinatura.",
+  },
+  {
+    id: "4",
+    question: "Vocês têm planos corporativos?",
+    answer:
+      "Com certeza! Você pode atualizar seu plano a qualquer momento e ainda recebe um desconto proporcional, garantindo o máximo valor da sua assinatura.",
+  },
+  {
+    id: "5",
+    question: "O que acontece quando eu fico sem armazenamento?",
+    answer:
+      "Com certeza! Você pode atualizar seu plano a qualquer momento e ainda recebe um desconto proporcional, garantindo o máximo valor da sua assinatura.",
+  },
+  {
+    id: "6",
+    question: "Posso atualizar meu plano?",
+    answer:
+      "Com certeza! Você pode atualizar seu plano a qualquer momento e ainda recebe um desconto proporcional, garantindo o máximo valor da sua assinatura.",
+  },
+  {
+    id: "7",
+    question: "Como convido minha equipe?",
+    answer:
+      "Com certeza! Você pode atualizar seu plano a qualquer momento e ainda recebe um desconto proporcional, garantindo o máximo valor da sua assinatura.",
+  },
+  {
+    id: "8",
+    question: "Vocês oferecem treinamento para indivíduos e equipes?",
+    answer:
+      "Com certeza! Você pode atualizar seu plano a qualquer momento e ainda recebe um desconto proporcional, garantindo o máximo valor da sua assinatura.",
+  },
+  {
+    id: "9",
+    question: "Estou bloqueado fora da minha conta, o que devo fazer?",
+    answer:
+      "Com certeza! Você pode atualizar seu plano a qualquer momento e ainda recebe um desconto proporcional, garantindo o máximo valor da sua assinatura.",
+  },
+];
+
+export const plans = [
+  {
+    id: "0",
+    title: "Core",
+    priceMonthly: 19,
+    priceYearly: 12,
+    caption: "Melhor para criadores solo",
+    features: [
+      "100MB de armazenamento em nuvem",
+      "100+ modelos de prompts",
+      "5 projetos",
+      "Suporte 24/7",
+    ],
+    icon: "/images/circle.svg",
+    logo: "/images/plan-1.png",
+  },
+  {
+    id: "1",
+    title: "Overdrive",
+    priceMonthly: 79,
+    priceYearly: 59,
+    caption: "Plano mais popular",
+    features: [
+      "Todos os recursos do Starter",
+      "1TB de armazenamento adicional",
+      "Projetos ilimitados",
+      "Análises",
+    ],
+    icon: "/images/triangle.svg",
+    logo: "/images/plan-2.png",
+  },
+  {
+    id: "2",
+    title: "Team",
+    priceMonthly: 39,
+    priceYearly: 29,
+    caption: "Exclusivo para equipes",
+    features: [
+      "Todos os recursos do Overdrive",
+      "10TB de armazenamento adicional",
+      "50% de desconto por membro",
+      "Colaboração em tempo real",
+    ],
+    icon: "/images/hexagon.svg",
+    logo: "/images/plan-3.png",
+  },
+];
